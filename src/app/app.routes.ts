b79c9f0554d8ee7ab6d9aa7c4b888e7df771270f
giovsalas-cell <giov.salas@duocuc.cl>
2026-06-15 21:15:34 -0400

    migracion pagina web a Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './components/inicio/inicio';
+import { Formulario } from './components/formulario/formulario';
+import { Categoria } from './components/categoria/categoria';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Inicio },
+  { path: 'formulario', component: Formulario },
+  { path: 'categoria/:tipo', component: Categoria },
+];
